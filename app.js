4db4b9e17d3387197fca558be11809a570afeb15
James <43918220+jamxu88@users.noreply.github.com>
2019-01-02 18:51:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("We up");
 });
-client.on("message", message => {
-  let jam = message.guild.roles.get("530156981542060043");
-  let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-  jam.edit({color: random})
+client.on("message", message => {\
+  function change() {
+    jam.edit({color: random});
+  }
+  i = 0
+  while (i < 1) {
+    let jam = message.guild.roles.get("530156981542060043");
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    setTimeout(change, 2000);
+  }
 });
 client.login(process.env.BOT_TOKEN);
