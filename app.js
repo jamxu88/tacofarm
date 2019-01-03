5d08699f4e302b5a93f2eba4822d9b98ba5a186e
James <43918220+jamxu88@users.noreply.github.com>
2019-01-03 17:16:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ client.on("message", (message) => {
   function change() {   
     let role = message.guild.roles.get("530156981542060043");
     let role2 = message.guild.roles.get("516392093854335018");
-    let role3 = message.guild.roles.get("530167708361293864");
-    let role4 = message.guild.roles.get("527684778905042944");
-    let role5 = message.guild.roles.get("527684779659886592");
-    let role6 = message.guild.roles.get("527684780049956875");
     let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     role.edit({color: random});
     role2.edit({color: random});
-    role3.edit({color: random});
-    role4.edit({color: random});
-    role5.edit({color: random});
-    role6.edit({color: random});
   }
   if (message.content.startsWith("<@530165753811304449>")) {
     setInterval(() => { change(); }, 1000);
