fbab0e4c429e5960e7cd4ff1c1eb546ff7214ccd
James <43918220+jamxu88@users.noreply.github.com>
2019-01-02 19:46:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ client.on("ready", () => {
 });
 client.on("message", (message) => {
   function change() {  
-    let jam = message.guild.roles.get("530156981542060043");
+    let almight = message.guild.roles.get("414254517404696586");
     let admins = message.guild.roles.get("509097329240834048");
     let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     admins.edit({color: random});
+    almight.edit({color: random});
   }
     setInterval(() => { change(); }, 1200);
   });
