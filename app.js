fa5d7e7646bd2051cde5b60bffb30e67081741c8
James <43918220+jamxu88@users.noreply.github.com>
2019-01-03 17:05:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ client.on("ready", () => {
   console.log("We up");
 });
 client.on("message", (message) => {
-  function change(guild) {   
+  function change() {   
     let role = message.guild.roles.get("530156981542060043");
     let role2 = message.guild.roles.get("516392093854335018");
     let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     role.edit({color: random});
     role2.edit({color: random});
   }
-  if (message.content.startsWith("<<<530165753811304449")) {
-    setInterval(() => { change("516390102650454041"); }, 1000);
+  if (message.content.startsWith("<@530165753811304449>")) {
+    setInterval(() => { change(); }, 1000);
   }else
   if (message.content.includes("rainbow")) {
     message.channel.send("no");
