5e77ecc40835064003d224e18bf77aa2ac461f2c
James <43918220+jamxu88@users.noreply.github.com>
2019-01-10 17:38:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ client.on("message", (message) => {
   console.log(message.author.id)
   function change() {   
     let role = message.guild.roles.get("527684781056458753");
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    role.edit({color: random});
-  }
+    role.edit({color: 10223871});
+    role.edit({color: 9442301});
+    role.edit({color: 8270329});
+    role.edit({color: 7293430});
+    role.edit({color: 6186482});
+    role.edit({color: 5079535});
+    role.edit({color: 3776746});
+    role.edit({color: 2735079});
   if (message.content.startsWith("<@530165753811304449>")) {
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
-    setInterval(() => { change(); }, 1000);
     setInterval(() => { change(); }, 1000);
   }else
   if (message.content.includes("rainbow")) {
